fb84e688b0a028c97704dcc3629b6a9854326bdf
jjtheall <jtheall@quinnipiac.edu>
2023-02-23 23:08:52 -0500

    Workspace Block Changes WorkspaceBlock.js 	-draggable component with bounds to stay in parent Sandbox.js 	-added WorkspaceBlock component in workspace div index.css 	-added styling for WorkspaceBlock 	-added styling for workspace div to make position:relative

--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -1,10 +1,14 @@
+import WorkspaceBlock from "./WorkspaceBlock";
+
 function Sandbox() {
   return (
     <div id="sandbox">
       <div className="mainTitleDiv">
         <h2 className="mainTitles">Sandbox</h2>
       </div>
-      <div id="workspace" className="contentDiv"></div>
+      <div id="workspace" className="contentDiv">
+        <WorkspaceBlock />
+      </div>
     </div>
   );
 }
