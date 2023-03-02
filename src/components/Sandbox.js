fbdd72f49bffc20e6e5dcbb9fbb10f9d2234952b
jjtheall <jtheall@quinnipiac.edu>
2023-03-02 09:52:57 -0500

    sandbox.js function to class rewrite, workspaceblock.js function to class rewrite and onstop/onstart progress

--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -1,5 +1,30 @@
 import WorkspaceBlock from "./WorkspaceBlock";
+import React from "react";
 
+class Sandbox extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <div id="sandbox">
+        <div className="mainTitleDiv">
+          <h2 className="mainTitles">Sandbox</h2>
+        </div>
+        <div id="workspace" className="contentDiv">
+          <WorkspaceBlock />
+          <WorkspaceBlock />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sandbox;
+
+
+/*
 function Sandbox() {
   return (
     <div id="sandbox">
@@ -7,10 +32,11 @@ function Sandbox() {
         <h2 className="mainTitles">Sandbox</h2>
       </div>
       <div id="workspace" className="contentDiv">
-        <WorkspaceBlock />
+        <WorkspaceBlock targetX={300} targetY={200}/>
       </div>
     </div>
   );
 }
 
 export default Sandbox;
+*/
