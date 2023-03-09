7895e1b1ccaf27a628c4a2f64effdd6d4dcf33c3
jjtheall <jtheall@quinnipiac.edu>
2023-03-09 18:40:10 -0500

    made edits to WorkspaceBlock.js and Sandbox.js for drag-drop functionality in an unordered list that roughly mimics desired snapping functionality. Using react-beautiful-dnd library

--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -1,42 +1,60 @@
 import WorkspaceBlock from "./WorkspaceBlock";
-import React from "react";
+import React, {useState} from "react";
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 
-class Sandbox extends React.Component{
-  constructor(){
-    super();
+const workspaceBlocks = [
+  {
+    name: "block 1"
+  },
+  {
+    name: "block 2"
+  },
+  {
+    name: "block 3"
   }
+]
 
-  render(){
-    return (
-      <div id="sandbox">
-        <div className="mainTitleDiv">
-          <h2 className="mainTitles">Sandbox</h2>
-        </div>
-        <div id="workspace" className="contentDiv">
-          <WorkspaceBlock />
-          <WorkspaceBlock />
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Sandbox;
-
-
-/*
 function Sandbox() {
+  const [blocks,updateBlocks] = useState(workspaceBlocks)
+
+  function handleOnDragEnd(result){
+    if(!result.destination) return;
+    const items = Array.from(blocks);
+    const [reorderedItem] = items.splice(result.source.index,1);
+    items.splice(result.destination.index,0,reorderedItem);
+
+    updateBlocks(items);
+  }
+
   return (
     <div id="sandbox">
       <div className="mainTitleDiv">
         <h2 className="mainTitles">Sandbox</h2>
       </div>
       <div id="workspace" className="contentDiv">
-        <WorkspaceBlock targetX={300} targetY={200}/>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="workspaceBlockList">
+            {(provided) => (
+              <ul className="workspaceBlockList" {...provided.droppableProps} ref={provided.innerRef}>
+              {blocks.map(({name},index) => {
+                return (
+                  <Draggable key={name} draggableId={name} index={index}>
+                    {(provided) => (
+                      <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                        <WorkspaceBlock name={name}/>
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     </div>
   );
 }
 
 export default Sandbox;
-*/
--- src/components/WorkspaceBlock.js
+++ src/components/WorkspaceBlock.js
@@ -1,64 +1,13 @@
 import React from "react";
-import Draggable, {DraggableCore} from 'react-draggable';
 
+function WorkspaceBlock(props){
+    let name = props.name
 
-class WorkspaceBlock extends React.Component{
-    constructor(){
-        super();
-        /*
-        this.state={
-            x:0,
-            y:0,
-            lastClicked:false
-        }
-        */
-        var lastClicked = false;
-    }
-
-    onStop = (e,data) => {
-        //allBlocks gets all workspace blocks
-        let allBlocks = document.querySelectorAll(".workspaceBlock");
-        
-        //iterate through allBlocks checking if any are close enough to snap with this one
-        for(let i=0;i<allBlocks.length;i++){
-             let curTransform = allBlocks[i].getAttribute("style");
-             var curX = curTransform.substring(curTransform.indexOf('(')+1,curTransform.indexOf('p'));
-             var curY = curTransform.substring(curTransform.indexOf('p')+3,curTransform.lastIndexOf('p'));
-             
-             if(data.x > curX - 30 && data.x < curX + 30 && data.y > curY - 30 && data.y < curY + 30){
-                console.log("snap")
-             }
-        }
-    }
-
-    onStart = (e,data) => {
-
-        let allBlocks = document.querySelectorAll(".workspaceBlock");
-        /*    
-        //iterate through allBlocks setting lastClicked to false on each
-        for(let i=0;i<allBlocks.length;i++){
-            allBlocks[i].getAttribute("state"); 
-        }
-
-        //set lastClicked to true on current block
-        this.setState({lastClicked:true});
-        */
-        //console.log(allBlocks[0]);
-        console.log(allBlocks[0]);
-        
-    }
-
-    render(){
-        return (
-            <Draggable bounds={"parent"} onStop={this.onStop} onStart={this.onStart}>
-                <div className="workspaceBlock">
-                    Drag me!
-                </div>
-            </Draggable>
-        );
-    }
+    return (
+        <div className="workspaceBlock">
+            {name}
+        </div>
+    );
 }
 
-
 export default WorkspaceBlock;
-
