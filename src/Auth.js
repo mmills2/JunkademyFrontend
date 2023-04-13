9ece74fc840485277325651a082e9b10e0f21545
James Gonsalves <jgon0201@gmail.com>
2023-04-13 13:31:17 -0400

    Editing Auth.js

--- src/Auth.js
+++ src/Auth.js
@@ -1,18 +1,18 @@
 import axios from 'axios'
 
-const INSTRUCTOR = 'in28minutes'
-const PASSWORD = 'dummy'
+const USER = 'test'
+const PASSWORD = 'junkademy2023'
 const API_URL = 'http://localhost:8080'
-const LOGIN_API_URL = `${API_URL}/instructors/${INSTRUCTOR}`
+const LOGIN_API_URL = `${API_URL}//${USER}`
 
-class CourseDataService {
+class DataService {
 
     retrieveAllCourses(name) {
         console.log('executed service')
-        return axios.get(`${LOGIN_API_URL}/courses`,
-            { headers: { authorization: 'Basic ' + window.btoa(INSTRUCTOR + ":" + PASSWORD) } }
+        return axios.get(`${LOGIN_API_URL}/user`,
+            { headers: { authorization: 'Basic ' + window.btoa(USER + ":" + PASSWORD) } }
         );
     }
 }
 
-export default new CourseDataService()
+export default new DataService()
